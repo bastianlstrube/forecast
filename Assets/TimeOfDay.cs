3ab96b5448c5ce28fe928f25a138aa40d2e3549f
Oscar <oscarlouw@gmail.com>
2017-09-04 18:22:34 +0200

    Last fluid sim push. RIP fluid :'(

--- Assets/TimeOfDay.cs
+++ Assets/TimeOfDay.cs
@@ -8,16 +8,28 @@ public class TimeOfDay : MonoBehaviour {
 
     public float timeOfDay;
 
+    private Quaternion originalRotation;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        originalRotation = directionalLight.transform.rotation;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         timeOfDay += timeSpeed;
 
-        directionalLight.transform.rotation = Quaternion.Euler(timeOfDay, 13, 0);
+        directionalLight.transform.rotation = Quaternion.Euler(timeOfDay, originalRotation.eulerAngles.y, originalRotation.eulerAngles.z);
 
+        if (directionalLight.transform.rotation.eulerAngles.x > 0.0f && directionalLight.transform.rotation.eulerAngles.x < 180.0f)
+        {
+            if (RenderSettings.ambientIntensity < 1.0f)
+                RenderSettings.ambientIntensity += 0.02f;
+        } else
+        {
+            if (RenderSettings.ambientIntensity > 0.5f)
+                RenderSettings.ambientIntensity -= 0.02f;
+        }
     }
 }
