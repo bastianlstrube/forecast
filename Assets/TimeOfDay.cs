cd88dcd4904b35071f1ac80c0aaa9263f8866641
Oscar <oscarlouw@gmail.com>
2017-09-04 13:49:06 +0200

    Time of day, particle tweaks

---
+++ Assets/TimeOfDay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeOfDay : MonoBehaviour {
+    public Light directionalLight;
+    public float timeSpeed = 1f;
+
+    public float timeOfDay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeOfDay += timeSpeed;
+
+        directionalLight.transform.rotation = Quaternion.Euler(timeOfDay, 13, 0);
+
+    }
+}
